a5baacaf84ed99b16d5207e01eae3014c23258a4
Sheb <michael@sheb.rocks>
2022-10-20 11:19:44 +0200

    usestate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
+const initialState = "Hello Everyone!"
 
 function App() {
+  // console.log(useState())
+  const [state, setState] = useState(initialState) // observable
+  const [count, setCount] = useState(0) // observable
+  // const [value, dispatch] = useStateHook(intialValue)
+
+  // const meineFunktion = function() {}
+
+  function increment(oldValue) {
+    return oldValue + 1
+  }
+
   return (
-    null
+    <div>
+      <p>{state}</p>
+      <button onClick={() => { 
+        console.log ('clicked')
+        // state = "anderer text"
+        setState("anderer text")
+      }}>change text</button>
+      <p>{count}</p>
+      <button onClick={() => {
+        setCount((currywurst) => currywurst + 1) 
+        setCount((alterWert) => {
+          const neuerWert = alterWert + 1
+          return neuerWert
+        }) 
+        setCount(increment)
+      }}>Increment Count</button>
+    </div>
   );
 }
 
